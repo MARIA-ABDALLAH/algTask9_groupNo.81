ae72a8d6616d0a75a66acb9cd57b9b5e7668de39
MARIA-ABDALLAH <mariasafwat37@gmail.com>
2023-04-25 10:28:42 +0200

    Create alg9_nonRe.c
    task 9 non recursive algorithm

---
+++ alg9_nonRe.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAXSIZE 8
+int main_1()
+{
+    int a[MAXSIZE], tmp;
+    printf("Enter your array enter\n");
+    for(int i=0; i<MAXSIZE; i++){
+        scanf("%d", &a[i]);
+    }
+    for(int i=0; i<MAXSIZE; i++){
+        if (a[i] > 0&& a[i+1]<0){
+            tmp=a[i];
+            a[i]=a[i+1];
+            a[i+1]=tmp;
+            i=i-2;
+        }
+    }
+    for(int i=0; i<MAXSIZE; i++)
+        printf("%d \t", a[i]);
+    return 0;
+}
