4b6a42477890980e9e6b0ebbc922634eec7e0e06
MARIA-ABDALLAH <105162021+MARIA-ABDALLAH@users.noreply.github.com>
2023-05-04 02:49:57 +0300

    Add files via upload

---
+++ alg9_nonRe.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAXSIZE 8
+int main()
+{
+    int a[MAXSIZE], tmp;
+    printf("Enter your array enter\n");
+    for(int i=0; i<MAXSIZE; i++){
+        scanf("%d", &a[i]);
+    }
+    /*for(int i=0; i<MAXSIZE; i++){
+        if (a[i] > 0 && a[i+1]<0){
+            tmp=a[i];
+            a[i]=a[i+1];
+            a[i+1]=tmp;
+            i=i-2;
+        }
+    }*/
+    int i=0;
+    while(++i<=MAXSIZE){
+        if (a[i-1] > 0 && a[i] < 0){
+            tmp= a[i-1];
+            a[i-1]=a[i];
+            a[i]=tmp;
+            i-=2;
+        }
+    }
+    for(int i=0; i<MAXSIZE; i++)
+        printf("%d \t", a[i]);
+    return 0;
+}
